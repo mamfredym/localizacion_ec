db5db46bc157eed4e7543813daa2f01e9220325b
Christopher Ormaza <chris.ormaza@gmail.com>
2020-06-17 13:55:58 -0500

    IMP: agreado de dependencias FIX: el orden de importacion de archivos para los valores default

--- l10n_ec_niif/models/__init__.py
+++ l10n_ec_niif/models/__init__.py
@@ -14,10 +14,10 @@ from . import l10n_ec_common_document_electronic
 from . import sri_error_code
 from . import sri_key_type
 from . import xml_data
+from . import company
 from . import account_move
 from . import account_journal
 from . import withhold
-from . import company
 from . import res_users
 from . import res_config
 from . import l10n_ec_utils
