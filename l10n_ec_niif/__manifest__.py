6c932407c248ca7b5944b6ade7981377b30a6c12
Christopher Ormaza <chris.ormaza@gmail.com>
2021-05-20 13:55:05 -0500

    IMP: agregado de concepto 3440 en formulario de declaración, corrección de movimientos contables, pendiente actualización SRI con respecto a json y xml

--- l10n_ec_niif/__manifest__.py
+++ l10n_ec_niif/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Ecuador - Accounting IFRS",
-    "version": "13.0.1.1.5",
+    "version": "13.0.1.1.6",
     "category": "Localization",
     "author": "Spearhead",
     "website": "https://www.spearhead.global",
