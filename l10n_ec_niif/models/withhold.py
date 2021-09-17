c46def7e8c7e9e602b481a2ec226332c171127e3
Carlos Lopez <celm1990@gmail.com>
2021-09-17 11:10:29 -0500

    FIX: cuando no se valida en linea con el SRI, marcar documento como valido
    complementario a commit 5be1ad32

--- l10n_ec_niif/models/withhold.py
+++ l10n_ec_niif/models/withhold.py
@@ -434,7 +434,10 @@ class L10nEcWithhold(models.Model):
     def _l10n_ec_action_validate_authorization_sri(self):
         # intentar validar el documento en linea con el SRI
         if self.type == "sale":
-            if (self.document_type == "pre_printed") and tools.config.get("validate_authorization_sri", True):
+            if not tools.config.get("validate_authorization_sri", True):
+                self.write({"l10n_ec_sri_authorization_state": "valid"})
+                return True
+            if self.document_type == "pre_printed":
                 if self.partner_authorization_id:
                     authorization_number = self.partner_authorization_id.number
                 else:
