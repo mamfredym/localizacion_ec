b5e4c309b8dc37737556bc43979996011305bf25
Carlos Lopez <celm1990@gmail.com>
2021-09-20 16:01:56 -0500

    FIX: quitar espacios a nombre de impuestos complementario a comit 90502a9c Esto provocaba que al importar productos haciendo referencia a impuestos no se asocien correctamenete xq el nombre tenia espacios

--- l10n_ec_niif/__manifest__.py
+++ l10n_ec_niif/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Ecuador - Accounting IFRS",
-    "version": "13.0.1.1.9",
+    "version": "13.0.1.2.1",
     "category": "Localization",
     "author": "Spearhead",
     "website": "https://www.spearhead.global",
